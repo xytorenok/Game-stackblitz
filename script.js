0c32595779f818ae986f67f59f47591dd94a0121
xytorenok <88131869+xytorenok@users.noreply.github.com>
2023-04-15 20:37:13 +0300

    1 file modified

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const stat = document.getElementById('status');
 const life = document.getElementById('life');
 const message = document.getElementById('message');
 const distancePoint = document.getElementById('distance');
+const cruiser = document.getElementById('cruiser');
 const rocket = document.getElementById('rocket');
+
+
 
 let crashes = 0;
 let lives = 5;
@@ -22,11 +25,10 @@ function jump() {
     dino.classList.remove('jump');
   }, 600);
 
-  if (daysClean == 4){
-    // rocket.style.left = '30%'
-    rocket.style.animation = 'rocket 5000ms linear'
+  if (daysClean == 1) {
+    rocket.style.left = '30%'
+    rocket.style.animation = 'rocket 5000ms linear';
   }
-
 }
 
 let crash = false;
@@ -39,10 +41,10 @@ let isAlive = setInterval(function () {
   );
   life.innerHTML = lives;
 
-//   счетчик пройденного пути
+  //   счетчик пройденного пути
   if (cactusLeft < 50 && cactusLeft > 0 && !start) {
-    start = true
-    daysClean ++;
+    start = true;
+    daysClean++;
     distancePoint.innerHTML = daysClean;
     // добавляем паузу в 1 секунду, чтобы счетчик не увеличивался слишком быстро
     setTimeout(function () {
@@ -55,13 +57,13 @@ let isAlive = setInterval(function () {
     crashes++;
     stat.innerHTML = crashes;
     lives--;
-    daysClean--;  
+    daysClean--;
 
     if (crashes == 4) {
       crashes = -1;
     }
 
-    // обнуляем пройденное растояние если жизни на нуле 
+    // обнуляем пройденное растояние если жизни на нуле
     if (lives == 0) {
       lives = 5;
       daysClean = -1;
@@ -74,5 +76,30 @@ let isAlive = setInterval(function () {
   }
 }, 10);
 
-//  message.hidden = false
+let rocketStrike = setInterval(function () {
 
+  let cruiserRight = parseInt(window.getComputedStyle(cruiser).getPropertyValue('right'));
+  let cruiserLeft = parseInt(window.getComputedStyle(cruiser).getPropertyValue('left'));
+
+console.log(`Корабль правое позиционирование: ${cruiserRight}`)
+console.log(`Корабль левое позиционирование: ${cruiserLeft}`)
+
+let rocketLeft = parseInt(window.getComputedStyle(rocket).getPropertyValue('left'));
+let rocketRight = parseInt(window.getComputedStyle(rocket).getPropertyValue('right'));
+
+console.log(`Рокета правое позиционирование: ${rocketRight}`)
+console.log(`Рокета левое позиционирование: ${rocketLeft}`)
+
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue('left')
+  );
+
+ 
+  if (rocketLeft < 50 && rocketLeft > 0 && !start) {
+    
+    
+  }
+
+  // (cactusLeft < 100 && cactusLeft > 50 && dinoTop >= 125 && !crash)
+
+}, 5500);
